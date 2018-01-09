a6914c4213523e069bdf39f6017ee61476434c67
Ravi Malik <ravimalik2364@gmail.com>
2018-01-09 16:42:30 -0500

    Basics: delete

---
+++ 6_delete.sql
@@ -0,0 +1,9 @@
+insert into person (fname, lname, gender, birth_date)
+Values ('Susan', 'Ortega', 'F', '1992-04-27');
+
+select * from person;
+
+delete from person
+where person_id >= 2;
+
+select * from person;
