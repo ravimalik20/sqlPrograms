5142472574400267ad43f7776d3f64023ac147a1
Ravi Malik <ravimalik2364@gmail.com>
2018-01-09 17:04:26 -0500

    Restructured repo && custom select in queries
    Restructured the data to distinguish basic concepts from intermediate
    concepts.
    
    Added code to demonstrate custom columns in select statement and
    also how to remove duplicates from the final result set.

---
+++ 2_Querying/1_custom_select.sql
@@ -0,0 +1,14 @@
+select emp_id, 'Active', emp_id * 3.14, UPPER(lname)
+from employee;
+
+select version(), user(), database();
+
+/* Column Aliases */
+select emp_id as Employee_ID,
+	'Active' as Status,
+	emp_id * 3.14 as Emp_id_x_pi,
+	UPPER(lname) as Lname_upper
+from employee;
+
+/* Remove duplicates */
+select distinct cust_id from account;
