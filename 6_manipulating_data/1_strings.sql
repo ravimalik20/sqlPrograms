cbee02f17295a869bc53d5f069e42dc2c60e6ec1
Ravi Malik <ravimalik2364@gmail.com>
2018-01-15 23:26:18 -0500

    Reorganised code.

--- 6_manipulating_data/1_strings.sql
+++ 6_manipulating_data/1_strings.sql
@@ -21,3 +21,20 @@ from (select 'ajklasjka is jkaskjsakj' vchar_fld) s;
 /* strcmp() -1 < ; 0 == ; 1 > */
 
 select strcmp('abc', 'abcd'), strcmp('abc', 'abc'), strcmp('bbc', 'abc');
+
+
+/* concat() */
+
+select concat(fname, ' ', lname) name from employee;
+
+/* insert(string, position, replace, word) */
+
+select fname, insert(fname, 4, 0, ' inserted ') inserted from employee;
+
+/* Replacing using insert */
+select fname, insert(fname, 3, 4, 'xx') classified from employee;
+
+
+/* substring(string, start, length) */
+
+select substring('goodbye cruel world', 9, 5);
